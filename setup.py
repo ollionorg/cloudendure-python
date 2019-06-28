bb992824b04baa1bca7e50f6cc470ed9a2ec7e9e
Mark Beacom <Beacom@gmail.com>
2019-06-28 19:45:48 -0400

    Pin 0.0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL: str = "https://github.com/mbeacom/cloudendure-python"
 EMAIL: str = "markvbeacom@gmail.com"
 AUTHOR: str = "Mark Beacom"
 REQUIRES_PYTHON: str = ">=3.6.0"
-VERSION: str = "0.0.4"
+VERSION: str = "0.0.5"
 
 REQUIRED: List[str] = ["requests", "boto3", "fire"]
 EXTRAS: Dict[str, List[str]] = {
